aba9120d4f603968ab5e0befef9f684d69b8ecda
Alex-Gichau <gichaumburu@gmail.com>
2020-10-05 19:49:28 +0300

    Activity 3 (Phone Intent) - Uri, Intent and textview click handler

--- src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.net.URI;
+
 public class InfoActivity extends AppCompatActivity {
 
     @Override
@@ -42,5 +44,13 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri URI = Uri.parse("tel:0123456789");
+        phoneIntent.setData(URI);
+        startActivity(phoneIntent);
+
+    }
+
     // ***
 }
